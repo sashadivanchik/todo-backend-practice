32ad560b52ebd449a7ce9b63b21b0f3e4ffa1e03
Alexander <alexanderslesarevpost@mail.ru>
2024-03-08 13:16:04 +0400

    feat: todoController, добавил метод получение списка дел из базы

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,9 +1,15 @@
-const {Todo} = require('../../models/models');
+const { Todo } = require('../../models/models');
 
 
 class TodoController {
     async getTodos(req, res) {
-
+        try {
+            const items = await Todo.findAll();
+            res.json(items);
+        } catch (error) {
+            console.error('Error fetching items:', error);
+            res.status(500).json({ error: 'Failed to fetch items' });
+        }
     }
 
     async addTodo(req, res) {
